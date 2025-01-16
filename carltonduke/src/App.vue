c0ceb13e0e61c5465955952f3c20d9f96c048481
Carlton Duke <carltondukeatx@gmail.com>
2025-01-15 23:33:08 -0600

    add site template

---
+++ carltonduke/src/App.vue
@@ -0,0 +1,124 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import BabylonScene from './components/BabylonScene.vue'
+import LinkButton from './components/LinkButton.vue';
+import AboutButton from './components/AboutButton.vue';
+import NameLogo from './components/NameLogo.vue';
+
+</script>
+
+<template>
+  <header class="bkg-main">
+    <div class="back">
+      <!---<h1 class="logo">Carlton</h1>
+      <h1 class="logo2">Duke</h1>--->
+      
+      <div class="link-area">
+        
+      </div>
+    </div>
+    <NameLogo></NameLogo>
+  </header>
+  <BabylonScene/>
+  <!---<RouterView />-->
+</template>
+
+<style scoped>
+.bkg-main {
+  background-color: rgb(33,33,33);
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  text-transform: uppercase;
+  color: #4CAF50;
+  font-family: Arial, Helvetica, sans-serif;
+  letter-spacing: 3px;
+  font-size:5rem;
+  margin: 0.1rem 0;
+}
+
+.logo2 {
+  text-transform: uppercase;
+  color: #474747;
+  font-family: Arial, Helvetica, sans-serif;
+  letter-spacing: 3px;
+  font-size:4rem;
+  margin: 0.1rem 0;
+}
+
+.back {
+  padding: 10px;
+	justify-content: right;
+  align-items: right;
+  border-right: 1px solid var(--color-border);
+}
+
+.link-area {
+  display:flex;
+  margin: 5px;
+}
+
+.link-button {
+  margin: 10px;
+  padding: 5px;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
+
+
---
+++ carltonduke/src/components/BabylonScene.vue
@@ -0,0 +1,18 @@
+<template>
+    <canvas ref="bjsCanvas" width="500" height="500" />
+</template>
+
+<script setup>
+    import { ref, onMounted } from "@vue/runtime-core";
+    import { createScene } from "../scenes/MyFirstScene";
+
+    // Reference to the canvas element
+    const bjsCanvas = ref(null);
+
+    // Lifecycle hook to initialize the scene
+    onMounted(() => {
+    if (bjsCanvas.value) {
+        createScene(bjsCanvas.value);
+    }
+    });
+</script>
